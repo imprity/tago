dffb017fe3c955079e1bafabb65736beb8511380
imprity <imprity041@gmail.com>
2025-03-07 10:22:05 +0900

    cleaned things up

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"os"
+	"log"
 	"path/filepath"
 	"slices"
 	"strings"
 	"unicode/utf8"
 )
+
+var ErrLogger *log.Logger = log.New(os.Stderr, "ERROR: ", log.Lshortfile)
+var WarnLogger *log.Logger = log.New(os.Stderr, "WARN: ", log.Lshortfile)
+var InfoLogger *log.Logger = log.New(os.Stdout, "INFO: ", log.Lshortfile)
 
 func printHelp() {
 	println("NOT IMPLEMENTED")
@@ -23,19 +28,33 @@ func main() {
 
 	var fileName = args[0]
 
-	if tagoFiles, err := findTagosForFile(fileName); err != nil {
-		panic(err)
-	} else if len(tagoFiles) > 0 {
+	tagoFiles, err := findTagosForFile(fileName)
 
-		fmt.Printf("tago files\n")
-		for _, file := range tagoFiles {
-			fmt.Printf("    %s\n", file)
+	// if we couldn't find any tago files
+	// just exit normally
+	if len(tagoFiles) <= 0 && err == nil{
+		fmt.Printf("could not find any tago files")
+		os.Exit(0)
+	}
+
+	// report errors
+	if err != nil {
+		if len(tagoFiles) <= 0 {
+			ErrLogger.Fatalf("could not find any tago files: %s", err)
+		}else {
+			WarnLogger.Printf("error while finding tago files: %s", err)
 		}
-		fmt.Printf("\n")
+	}
 
+	fmt.Printf("tago files:\n")
+	for _, file := range tagoFiles {
+		fmt.Printf("    %s\n", file)
+	}
+	fmt.Printf("\n")
+
+	// print key values from tago files
+	{
 		keyValue := make(map[string]string)
-
-		//for _, file := range tagoFiles {
 		for i := len(tagoFiles) - 1; i >= 0; i-- {
 			file := tagoFiles[i]
 			fileText, err := os.ReadFile(file)
@@ -54,8 +73,6 @@ func main() {
 		}
 
 		printKeyValue(keyValue)
-	} else {
-		fmt.Printf("couldn't find tago file for %s\n", fileName)
 	}
 }
 
@@ -81,19 +98,27 @@ func isTagoFile(path string) bool {
 	return false
 }
 
+func fileExistsAndRegular(path string) error {
+	// check if file exists
+	info, err := os.Stat(path)
+
+	if err == nil { // file exists
+		mode := info.Mode()
+		if !mode.IsRegular() { // but it's not regular
+			return fmt.Errorf("%s is not a regular file", path)
+		}
+		return nil
+	}
+
+	return err
+}
+
 // returns empty path if it couldn't find any
 func findTagosForFile(filePath string) ([]string, error) {
-	{
-		// check if file even exists
-		info, err := os.Stat(filePath)
-		if err != nil {
-			return nil, err
-		}
+	filePath = filepath.Clean(filePath) // why not
 
-		// check if file is regular
-		if !info.Mode().IsRegular() {
-			return nil, fmt.Errorf("%s is not a regular file", filePath)
-		}
+	if err := fileExistsAndRegular(filePath); err != nil {
+		return nil, err
 	}
 
 	var tagos []string
@@ -101,7 +126,10 @@ func findTagosForFile(filePath string) ([]string, error) {
 	// get abs
 	fileAbsPath, err := filepath.Abs(filePath)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf(
+			"could not get absolute path of %s: %w",
+			filePath, err,
+		)
 	}
 
 	fileDir := filepath.Dir(fileAbsPath)
@@ -134,15 +162,21 @@ func findTagosForFile(filePath string) ([]string, error) {
 
 		name, _ := getNameAndExt(dirent.Name())
 
-		// if we find tago file with the same name, that is the tago file
+		// we found tago file with the same name
 		if name == fileName {
 			fileTago = direntPath
 		}
 
-		// instead if we root.tago file, that is the tago file
-		if name == "root" {
+		// if we found tago.tago file
+		if name == "tago" {
 			rootTago = direntPath
 		}
+	}
+
+	if isTagoFile(fileAbsPath) {
+		// if file it self is a tago file
+		// add it to tagos
+		fileTago = fileAbsPath
 	}
 
 	if len(fileTago) > 0 {
@@ -179,7 +213,8 @@ func findTagosForFile(filePath string) ([]string, error) {
 
 			name, _ := getNameAndExt(dirent.Name())
 
-			if name == "root" {
+			// we found tago.tago file
+			if name == "tago" {
 				tagos = append(tagos, direntPath)
 				foundRootTago = true
 				break
